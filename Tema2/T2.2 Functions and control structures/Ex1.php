5e48df7dcd35d489253514c86b5bd605049b5f63
RBJCTNN29 <62948217+RBJCTNN29@users.noreply.github.com>
2023-01-25 11:29:35 +0100

    Updated T2.2>Ex1

---
+++ Tema2/T2.2 Functions and control structures/Ex1.php
@@ -0,0 +1,15 @@
+<?php 
+    function even_or_odd($number) {
+        if($number % 2 == 0) {
+            echo "El nombre es parell"
+        }
+        else {
+            echo "El nombre es imparell"
+        }
+    }
+    echo "Aquesta funcio ens diu si un nombre es parell o imparell";
+    $num1 = 29;
+    $num2 = 30;
+    echo even_or_odd($num1);
+    echo even_or_odd($num2);
+?>
