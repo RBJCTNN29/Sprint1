e73d6e4790040c82979a86855f9df7ec8522989a
RBJCTNN29 <62948217+RBJCTNN29@users.noreply.github.com>
2023-01-25 12:15:24 +0100

    Fixed syntax in function

--- Tema2/T2.2 Functions and control structures/Ex5.php
+++ Tema2/T2.2 Functions and control structures/Ex5.php
@@ -3,7 +3,7 @@
         $division = "";
         if($grade >= 60) {
             $division = "Primera Divisio";
-        } else if(($grade < 60 <) and ($grade >= 45)) {
+        } else if(($grade < 60) and ($grade >= 45)) {
             $division = "Segona Divisio";
         } else if(($grade < 45) and ($grade >= 33)) {
             $division = "Tercera Divisio";
