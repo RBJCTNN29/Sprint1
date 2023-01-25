afd597c97c2578e18b783a7f0f7dcfc9dc725352
RBJCTNN29 <62948217+RBJCTNN29@users.noreply.github.com>
2023-01-25 12:43:14 +0100

    Fixing Ex1 no output

--- Tema3/Ex1/Sprint1_T3_Ex1.php
+++ Tema3/Ex1/Sprint1_T3_Ex1.php
@@ -7,16 +7,16 @@
         }
         function print() {
             if ($wage > 6000) {
-                echo $name." has to pay tax";
+                echo $name . " has to pay tax";
             }
             else {
-                echo $name." doesn't have to pay tax";
+                echo $name . " doesn't have to pay tax";
             }
         }
     }
 
     $employee1 = new Employee; 
     $employee1->initialize("Rory", 5999);
-    echo 
+    print($employee1);
 
 ?>
