6b331e46462906289119439ba596cc786e36c201
RBJCTNN29 <62948217+RBJCTNN29@users.noreply.github.com>
2023-01-25 14:37:14 +0100

    Fixed missing reference to object in function call

--- Tema3/Ex1/Sprint1_T3_Ex1.php
+++ Tema3/Ex1/Sprint1_T3_Ex1.php
@@ -5,18 +5,16 @@
             $this->name = $name;
             $this->wage = $wage;
         }
-        function print($this->wage) {
+        function print($employee) {
             if ($this->wage > 6000) {
-                echo "The student has to pay tax";
+                echo $this->name . " has to pay tax";
             }
             else {
-                echo "The student doesn't have to pay tax";
+                echo $this->name . " doesn't have to pay tax";
             }
         }
     }
-
     $employee1 = new Employee; 
     $employee1->initialize("Rory", 5999);
-    print($employee1);
-
+    $employee1->print($employee1);
 ?>
