2bc07df772b52cef3f21c621f62e522c9ef95a0a
RBJCTNN29 <62948217+RBJCTNN29@users.noreply.github.com>
2023-01-25 11:34:31 +0100

    Fine-tuning Ex1

--- Tema2/T2.2 Functions and control structures/Ex1.php
+++ Tema2/T2.2 Functions and control structures/Ex1.php
@@ -1,17 +1,17 @@
 <?php 
     function even_or_odd($number) {
         if($number % 2 == 0) {
-            echo "El nombre es parell";
+            echo "El nombre es parell" . "<br/>";
         }
         else {
-            echo "El nombre es imparell";
+            echo "El nombre es imparell" . "<br/>";
         }
     }
-    echo "Aquesta funcio ens diu si un nombre es parell o imparell";
+    echo "Aquesta funcio ens diu si un nombre es parell o imparell" . "<br/>";
     $num1 = 29;
-    echo "El primer nombre es el 29";
+    echo "El primer nombre es el 29" . "<br/>";
     echo even_or_odd($num1);
     $num2 = 30;
-    echo "El segon nombre es el 30";
+    echo "El segon nombre es el 30" . "<br/>";
     echo even_or_odd($num2);
 ?>
