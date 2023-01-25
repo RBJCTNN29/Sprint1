84e665a0fe25ada55419efe093faffb148883bde
RBJCTNN29 <62948217+RBJCTNN29@users.noreply.github.com>
2023-01-25 12:34:22 +0100

    Fine-tuning logic

--- Tema2/T2.2 Functions and control structures/Ex6.php
+++ Tema2/T2.2 Functions and control structures/Ex6.php
@@ -1,11 +1,13 @@
 <?php
     function isBitten() {
         $random = rand(0, 100);
-        $boolean_output = false;
+        $boolean_output = "";
         if($random  % 2 == 0) {
-            $boolean_output = $random? "TRUE" : "FALSE";
-            echo $boolean_output;
+            $boolean_output = "TRUE";
+        } else {
+            $boolean_output = "FALSE;"
         }
+        echo $boolean_output;
     }
     echo isBitten();
 ?>
