14ba5a792a57337e6abd49893803ccf68e1cdc64
RBJCTNN29 <62948217+RBJCTNN29@users.noreply.github.com>
2023-01-25 11:41:12 +0100

    Updated Ex2

--- Tema2/T2.2 Functions and control structures/Ex2.php
+++ Tema2/T2.2 Functions and control structures/Ex2.php
@@ -1,3 +1,10 @@
 <?php
-
+    function count_to_ten_in_twos() {
+        $current_num = 0;
+        while ($current_num != 12) {
+            echo $current_num;
+            $current_num += 2;
+        }
+    }
+    echo count_to_ten_in_twos();
 ?>
