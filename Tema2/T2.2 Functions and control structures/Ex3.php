d53bbf6fe66832802ef519976ae294881d8b8dfe
RBJCTNN29 <62948217+RBJCTNN29@users.noreply.github.com>
2023-01-25 11:55:11 +0100

    Updated Ex3

---
+++ Tema2/T2.2 Functions and control structures/Ex3.php
@@ -0,0 +1,11 @@
+<?php
+    function count_to_given_number($number) {
+        $current_num = 0;
+        while ($current_num != 12) {
+            echo $current_num . "<br/>";
+            $current_num += 1;
+        }
+    }
+    echo "Aquesta funcio compta fins al nombre donat com a argument (parametre). Per exemple escollim el 13" . "<br/>";
+    echo count_to_given_number(13);
+?>
