29f3eafa5ce071c78cf840b456f429e191bd53a0
RBJCTNN29 <62948217+RBJCTNN29@users.noreply.github.com>
2023-01-26 13:00:52 +0100

    working on Ex1 Level2

--- Tema3/Ex1 Nivell2/Ex1Nivell2.php
+++ Tema3/Ex1 Nivell2/Ex1Nivell2.php
@@ -1,60 +1,48 @@
 <?php
     class PokerDice {
-        public $faceA, $faceK, $faceQ, $faceJ, $face7, $face8;
-        public function __construct($faceA, $faceK, $faceQ, $faceJ, $face7, $face8) {
-            $this->faceA = "Ace";
-            $this->faceK = "King";
-            $this->faceQ = "Queen";
-            $this->faceJ = "Jack";
-            $this->face7 = "7";
-            $this->face8 = "8";
+        private $sideFacingUp;
+        public function __construct($sideFacingUp = null){
+            $this->sideFacingUp = $sideFacingUp;
         }
-        public function shapeName($faceName) {
-            echo "The die landed on " . $faceName;
+        public function throw() {
+            $this->sideFacingUp = rand(1, 6);
+            $randNum = $this->sideFacingUp;
+            $this->shapeName($randNum);
         }
-        public function throw($die) {
-            $randNum = rand(1, 6);
-            $dieFace;
-            switch($randFace) {
+        public function shapeName($randNum) {
+            switch($randNum) {
                 case 1:
-                    $dieFace = $this->faceA;
-                    shapeName($dieFace);
+                    echo "The die landed on Ace" . "<br/>";
                     break;
-                case 2:
-                    $dieFace = $this->faceK;
-                    shapeName($dieFace);
+                case 2: 
+                    echo "The die landed on King" . "<br/>";
                     break;
                 case 3:
-                    $dieFace = $this->faceQ;
-                    shapeName($dieFace);
+                    echo "The die landed on Queen" . "<br/>";
                     break;
                 case 4:
-                    $dieFace = $this->faceJ;
-                    shapeName($dieFace);
+                    echo "The die landed on Jack" . "<br/>";
                     break;
                 case 5:
-                    $dieFace = $this->face7;
-                    shapeName($dieFace);
+                    echo "The die landed on 7" . "<br/>"; 
                     break;
-                case 6:
-                    $dieFace = $this->face8;
-                    shapeName("8");
+                case 6: 
+                    echo "The die landed on 8" . "<br/>";
                     break;
             }
         }
-        public function castFive() {
+        public function throwFive() {
+            echo "Throwing five poker dice at a time" . "<br/>";
             for ($x = 0; $x <= 5; $x++) {
-                echo throw($die);
+                $this->throw();
             }
         }
-        public function getTotalThrows() {
-
+        public function getTotalThrows(){
+           
         }
     }
     $die1 = new PokerDice;
-    $die1->__construct($faceA, $faceK, $faceQ, $faceJ, $face7, $face8);
-    $die1->throw($die1);
-    $die1->castFive();
-
-
+    $die1->__construct();
+    $die1->throw();
+    $die1->throwFive();
 ?>
