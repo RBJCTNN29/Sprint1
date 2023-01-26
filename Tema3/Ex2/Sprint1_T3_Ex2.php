316800b83ad150d1396657da77999ac49d7104c2
RBJCTNN29 <62948217+RBJCTNN29@users.noreply.github.com>
2023-01-26 09:13:32 +0100

    Fixed subclass methods

--- Tema3/Ex2/Sprint1_T3_Ex2.php
+++ Tema3/Ex2/Sprint1_T3_Ex2.php
@@ -1,14 +1,33 @@
 <?php 
 class Shape {
     public $width, $height;
-    function __construct($width, $height) {
+    public function construct($width, $height) {
         $this->width = $width;
         $this->height = $height;
     }
 }
 class Triangle extends Shape {
-    function area(){
-        echo ($this->height * $this->width) / 2;
+    public function construct($width, $height) {
+        parent::construct($width, $height);
+    }
+    public function area($shape){
+        echo "L'area d'aquest triangle es de " . 
+        ($this->height * $this->width) / 2 . "<br/>";
     }
 }
+class Rectangle extends Shape {
+    public function construct($width, $height) {
+        parent::construct($width, $height);
+    }
+    public function area($shape) {
+        echo "L'area d'aquest rectangle es de " . 
+        ($this->height * $this->width);
+    }
+}
+$triangle1 = new Triangle;
+$triangle1->construct(4, 6);
+$triangle1->area($triangle1);
+$rectangle1 = new Rectangle;
+$rectangle1->construct(7,5);
+$rectangle1->area($rectangle1);
 ?>
