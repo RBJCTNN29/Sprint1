c4a4dfabe9d88ade1259a6bd8efc859e79a83e69
RBJCTNN29 <62948217+RBJCTNN29@users.noreply.github.com>
2023-02-02 10:27:48 +0100

    Kata 2 feb

---
+++ Tema3/Kata/Kata.php
@@ -0,0 +1,16 @@
+<?php
+$array1 = [ "ccc", "dddd", "a", "bb"];
+$array2 = ["1", "4444", "22", "333"];
+$array3 = ["cat", "mouse", "terrier", "falcon"];
+$matrix1 = [$array1, $array2, $array3];
+    function sort($matrix) {
+        foreach ($matrix as $array) {
+            foreach($array as $value){
+                return strlen($value)
+            }
+        }
+        return strlen()
+    }
+
+echo sort($matrix);
+?>
