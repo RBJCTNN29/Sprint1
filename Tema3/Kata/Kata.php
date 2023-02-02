b302316b53b147513245eb4a329e6a4f46eb71a1
RBJCTNN29 <62948217+RBJCTNN29@users.noreply.github.com>
2023-02-02 11:34:10 +0100

    Kata

--- Tema3/Kata/Kata.php
+++ Tema3/Kata/Kata.php
@@ -3,14 +3,10 @@ $array1 = [ "ccc", "dddd", "a", "bb"];
 $array2 = ["1", "4444", "22", "333"];
 $array3 = ["cat", "mouse", "terrier", "falcon"];
 $matrix1 = [$array1, $array2, $array3];
-    function sort($matrix) {
-        foreach ($matrix as $array) {
-            foreach($array as $value){
-                return strlen($value)
-            }
-        }
-        return strlen()
-    }
-
+function sort($matrix) {
+    return array_multisort(array_map("strlen", $array), $array);
+                
+}
+    
 echo sort($matrix);
 ?>
